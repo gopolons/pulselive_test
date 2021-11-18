2ec0b2c83ab489e728e175968521ac14df7d61c6
Georgii Polonskii <gopolons@gmail.com>
2021-11-18 13:29:47 +0000

    Initial Commit

---
+++ pulselive_testUITests/pulselive_testUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  pulselive_testUITestsLaunchTests.swift
+//  pulselive_testUITests
+//
+//  Created by Georgy Polonskiy on 18/11/2021.
+//
+
+import XCTest
+
+class pulselive_testUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
